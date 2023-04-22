0f57dd2b53b37e1826a45709b81d3b71ecac8064
LEOTARDAS <leonardas.varanavicius@gmail.com>
2023-04-23 00:42:30 +0300

    general styling

--- resources/views/conference/create.blade.php
+++ resources/views/conference/create.blade.php
@@ -1,37 +1,40 @@
 @extends('layouts.app')
-
+<link href="{{ asset('css/main.css') }}" rel="stylesheet">
 @section('content')
-    <h1>Create Conference</h1>
+
+<div class="container-main">
+    <div class="container-conf">
+    <a>Create Conference</a>
     <form method="POST" action="{{ route('conferences.store') }}">
         @csrf
-
+<br>
         <div>
-            <label for="title">Title:</label>
+            <label for="title">Title:</label><br>
             <input type="text" name="title" id="title" value="{{ old('title') }}">
         </div>
-
+        <br>
         <div>
-            <label for="description">Description:</label>
+            <label for="description">Description:</label><br>
             <textarea name="description" id="description">{{ old('description') }}</textarea>
         </div>
-
+        <br>
         <div>
-            <label for="start_time">Start Time:</label>
+            <label for="start_time">Start Time:</label><br>
             <input type="datetime-local" name="start_time" id="start_time" value="{{ old('start_time', date('Y-m-d\TH:i')) }}">
         </div>
-
+        <br>
         <div>
-            <label for="end_time">End Time:</label>
+            <label for="end_time">End Time:</label><br>
             <input type="datetime-local" name="end_time" id="end_time" value="{{ old('end_time', date('Y-m-d\TH:i')) }}">
         </div>
-        
+        <br>
         <div>
-            <label for="location">Location:</label>
+            <label for="location">Location:</label><br>
             <input type="text" name="location" id="location" value="{{ old('location') }}">
         </div>
-
+        <br>
         <div>
-            <label for="city">City:</label>
+            <label for="city">City:</label><br>
             <select name="city" id="city">
                 <option value="vilnius">Vilnius</option>
                 <option value="kaunas">Kaunas</option>
@@ -39,10 +42,15 @@
                 <option value="siauliai">Siauliai</option>
                 <option value="panevezys">Panevezys</option>
             </select>
-        </div>
-
+        </div><br><br>
+        @guest
+        <p>Log in to create a conference</p>
+        @else
         <div>
             <button type="submit">Create</button>
         </div>
+        @endguest
     </form>
+</div>
+</div>
 @endsection
--- resources/views/conference/edit.blade.php
+++ resources/views/conference/edit.blade.php
@@ -8,29 +8,29 @@
             @csrf
             @method('PUT')
             <div class="form-group">
-                <label for="title">Title</label>
+                <label for="title">Title</label><br>
                 <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $conference->title) }}">
-            </div>
+            </div><br>
             <div class="form-group">
-                <label for="description">Description</label>
+                <label for="description">Description</label><br>
                 <textarea class="form-control" id="description" name="description">{{ old('description', $conference->description) }}</textarea>
-            </div>
+            </div><br>
             <div class="form-group">
-                <label for="start_time">Start Time</label>
+                <label for="start_time">Start Time</label><br>
                 <input type="datetime-local" class="form-control" id="start_time" name="start_time" value="{{ old('start_time', $conference->start_time) }}">
-            </div>
+            </div><br>
             <div class="form-group">
-                <label for="end_time">End Time</label>
+                <label for="end_time">End Time</label><br>
                 <input type="datetime-local" class="form-control" id="end_time" name="end_time" value="{{ old('end_time', $conference->end_time) }}">
             </div>
-
+            <br>
             <div>
-                <label for="location">Location:</label>
+                <label for="location">Location:</label><br>
                 <input type="text" name="location" id="location" value="{{ old('location', $conference->location) }}">
             </div>
-
+            <br>
             <div>
-                <label for="city">City:</label>
+                <label for="city">City:</label><br>
                 <select name="city" id="city">
                     <option value="vilnius" {{ $conference->city == 'vilnius' ? 'selected' : '' }}>Vilnius</option>
                     <option value="kaunas" {{ $conference->city == 'kaunas' ? 'selected' : '' }}>Kaunas</option>
@@ -38,10 +38,13 @@
                     <option value="siauliai" {{ $conference->city == 'siauliai' ? 'selected' : '' }}>Siauliai</option>
                     <option value="panevezys" {{ $conference->city == 'panevezys' ? 'selected' : '' }}>Panevezys</option>
                 </select>
-            </div>
-
+            </div><br><br>
+            @guest
+            <p>Log in to edit</p>
+            @else
             <button type="submit" class="btn btn-primary">Update Conference</button>
-        </form>
+            @endguest
+        </form><br>
         <a href="{{ route('conferences.index') }}"><button>Back</button></a>
         </div>
     </div>
